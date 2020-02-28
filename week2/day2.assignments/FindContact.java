64d2169d16a48bc404170de5a5d4bd21562ff32d
TestLeafPages <sarath.muthuswamy@testleaf.com>
2020-02-28 11:15:19 +0530

    Add files via upload

---
+++ week2/day2.assignments/FindContact.java
@@ -0,0 +1,48 @@
+package week2.day2.Assignments;
+
+public class FindContact {
+
+	public static void main(String[] args) {
+		/*
+		 * //Pseudo Code
+		 * 
+		 * 1. Launch URL "http://leaftaps.com/opentaps/control/login"
+		 * 
+		 * 2. Enter UserName and Password Using Id Locator
+		 * 
+		 * 3. Click on Login Button using Class Locator
+		 * 
+		 * 4. Click on CRM/SFA Link
+		 * 
+		 * 5. Click on contacts Button
+		 * 
+		 * 6. Click on Find Contacts
+		 * 
+		 * 7. Click on the Email Field using Xpath Locator
+		 *
+		 * 8. Enter the email using any locator
+		 * 
+		 * 9. Click on Find Contacts using Xpath Locator
+		 * 
+		 * 10. Click on the First Resulting Contact
+		 * 
+		 * 11. Get the Text of First Name 
+		 * 
+		 * 12. Click on the Edit Button
+		 * 
+		 * 13. Select New Marketing Campaign as eCommerce Site Internal Campaign by using SelectByValue
+		 * 
+		 * 14. Click on the add button
+		 * 
+		 * 15. Enter Department Value as Selenium Automation Testing
+		 * 
+		 * 16. Click on the Update Button
+		 * 
+		 * 17. Get the text of Marketing campaign and Verify it
+		 * 
+		 * 18. verify Title of the Current page
+		 */
+
+	}
+
+}

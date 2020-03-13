e5340cb57a3b5b8f6794b51ee1c2512ecf1e12cf
TestLeafPages <sarath.muthuswamy@testleaf.com>
2020-03-13 15:02:13 +0530

    Add files via upload

---
+++ week4.day1/Assignment/ReverseMap.java
@@ -0,0 +1,17 @@
+// Insert elements to the TreeMap
+
+Input : treemap.put("1", "Welcome");
+             treemap.put("2", "to");
+             treemap.put("3", "TestLeaf");
+             
+
+// Elements should be printed in reverse order of their insertion
+
+Output : 3: TestLeaf
+         2: to
+         1: Welcome
+
+Psuedo Code
+
+
+		
